93f88e80a2c198e31cb8fd30e0dd6c2d569404c3
Nick Botelho <41928812+NickUltracraft@users.noreply.github.com>
2018-08-12 15:58:25 -0300

    Atualizar a Main principal
    Aqui seria adicionado o setupDependencies() que iria identificar qual plugin está sendo utilizado e assim ativar os eventos de cada plugin

--- src/main/java/com/gcunha/authmeCaptcha/AuthmeCaptcha.java
+++ src/main/java/com/gcunha/authmeCaptcha/AuthmeCaptcha.java
@@ -1,33 +1,56 @@
 package com.gcunha.authmeCaptcha;
 
-import com.gcunha.authmeCaptcha.events.EventPreLogin;
+import com.gcunha.authmeCaptcha.events.EventPreLoginAUTHME;
+import com.gcunha.authmeCaptcha.events.EventPreLoginNLOGIN;
 import com.gcunha.authmeCaptcha.events.EventShouldCancel;
 import com.gcunha.authmeCaptcha.manager.CaptchaManager;
 import com.gcunha.authmeCaptcha.manager.CommandManager;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
+import com.gcunha.authmeCaptcha.manager.PluginType.Plugin;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuthmeCaptcha extends JavaPlugin {
 
     private static AuthmeCaptcha instance;
-
     private CaptchaManager captchaManager;
+    public static Plugin plugin;
 
     @Override
     public void onEnable() {
         instance = this;
-
+        
+        setupDependencies();
         captchaManager = new CaptchaManager();
         setupEvents();
         new CommandManager();
 
     }
-
-    private void setupEvents(){
-        registerEvent(new EventPreLogin());
+    private void setupDependencies() {
+    	PluginManager pm = Bukkit.getPluginManager();
+    	if(pm.getPlugin("nLogin") != null) {
+    		plugin = Plugin.NLOGIN;
+    	}
+    	if(pm.getPlugin("AuthMe") != null) {
+    		plugin = Plugin.AUTHME;
+    	}
+    	if(pm.getPlugin("nLogin") == null && (pm.getPlugin("AuthMe") == null)) {
+    		plugin = Plugin.NAO_ENCONTRADO;
+    	}
+    }
+    private void setupEvents() {
+    	if(plugin == Plugin.NAO_ENCONTRADO) {
+    		Bukkit.getConsoleSender().sendMessage("[AuthmeCaptcha] Nenhum plugin de login encontrado. Desligando plugin...");
+    		return;
+    	}
+    	if(plugin == Plugin.NLOGIN) {
+    		registerEvent(new EventPreLoginNLOGIN());
+    	}
+    	if(plugin == Plugin.AUTHME) {
+    		registerEvent(new EventPreLoginAUTHME());
+    	}
         registerEvent(new EventShouldCancel());
     }
 
@@ -38,8 +61,6 @@ public class AuthmeCaptcha extends JavaPlugin {
     private void registerEvent(Listener event){
         getServer().getPluginManager().registerEvents(event , getInstance());
     }
-
-
     public static AuthmeCaptcha getInstance() {
         return instance;
     }
