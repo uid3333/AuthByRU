5b7d08a4b40be779faf607898e17e74a73b46de7
Nick Botelho <41928812+NickUltracraft@users.noreply.github.com>
2018-08-12 16:00:45 -0300

    Atualizar o Captcha Manager
    Aqui todos os métodos do captcha seriam colocados aqui

--- src/main/java/com/gcunha/authmeCaptcha/manager/CaptchaManager.java
+++ src/main/java/com/gcunha/authmeCaptcha/manager/CaptchaManager.java
@@ -1,12 +1,27 @@
 package com.gcunha.authmeCaptcha.manager;
 
+import com.gcunha.authmeCaptcha.AuthmeCaptcha;
 import com.gcunha.authmeCaptcha.Color;
 
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
 import java.util.HashMap;
+import java.util.Random;
 import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class CaptchaManager {
 
+    final protected AuthmeCaptcha plugin = AuthmeCaptcha.getInstance();
+
+    private Color[] cores = Color.values();
+    private Random random = new Random();
+    
     private HashMap<UUID , Color> captchaPlayers;
 
      public CaptchaManager(){
@@ -29,6 +44,46 @@ public class CaptchaManager {
 
     public boolean hasCaptcha(UUID uuid){
          return captchaPlayers.containsKey(uuid);
+    }
+    private TextComponent getColorTextComponent(Color color){
+        TextComponent msg = new TextComponent(ChatColor.translateAlternateColorCodes('&', color.getText()));
+        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§7Clique para selecionar!").create()));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/color " + color.getText().substring(4)));
+        return msg;
+    }
+    final Color getRandom(){
+        return cores[random.nextInt(cores.length)];
+    }
+    public void enviarTeste(Player player) { 
+    	Color color = getRandom();
+
+        plugin.getCaptchaManager().addCaptchaPlayer(player.getUniqueId(), color);
+
+        player.sendMessage(" ");
+        player.sendMessage(" ");
+        player.sendMessage(" ");
+
+        String textAsk = "&6Para poder jogar, faça o teste, clique na cor %cor%";
+        textAsk = textAsk.replace("%cor%",color.getText());
+
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&',textAsk));
+        player.sendMessage(" ");
+
+        TextComponent msg = new TextComponent("   ");
+        msg.addExtra(getColorTextComponent(Color.AZUL));
+        msg.addExtra("   ");
+        msg.addExtra(getColorTextComponent(Color.VERMELHO));
+        msg.addExtra("   ");
+        msg.addExtra(getColorTextComponent(Color.VERDE));
+        msg.addExtra("   ");
+        msg.addExtra(getColorTextComponent(Color.ROSA));
+
+        player.spigot().sendMessage(msg);
+        player.sendMessage(" ");
+
+        player.setWalkSpeed(0);
+        player.setFlySpeed(0);
+
     }
 
 }
